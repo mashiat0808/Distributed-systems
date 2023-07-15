d0f949ad0e0e1509ec25a41bc1f748d3ac2346f5
mashiat0808 <59837488+mashiat0808@users.noreply.github.com>
2023-07-16 00:26:42 +0600

    first demo

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <div className='hello'>
+        
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' ></Route>
+          
+        </Routes>
+          </BrowserRouter>
+          hello
+        </div>
+    </>
+  )
+}
+
+export default App
